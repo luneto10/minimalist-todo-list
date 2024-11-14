baeee9206652c9fa6229977d4a4552dd83ca16a0
luneto10 <lguedesdecarvalhon2@huskers.unl.edu>
2024-11-14 01:54:36 -0600

    Testing

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:5018",
+                target: "https://minimaltodolistapi.azurewebsites.net",
                 changeOrigin: true,
             },
         },
